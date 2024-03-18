0cae165f66492f5519a6582adc86567f92f70404
Farrukh Uralov <farhodovich29@gmail.com>
2024-03-18 10:19:55 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let request = +prompt("Salom! Bu raqamni oyga konvertatsiya qilib berivchi oyna. Iltimos 1dan 12gacha raqam kiriting: ");
+
+if(request == 1){
+    alert("Bu Yanvar oyi")
+}else if(request == 2){
+    alert("Bu Fevral oyi")
+}else if(request == 3){
+    alert("Bu Mart oyi")
+}else if(request == 4){
+    alert("Bu Aprel oyi")
+}else if(request == 5){
+    alert("Bu May oyi")
+}else if(request == 6){
+    alert("Bu Iyun oyi")
+}else if(request == 7){
+    alert("Bu Iyul oyi")
+}else if(request == 8){
+    alert("Bu Avgust oyi")
+}else if(request == 9){
+    alert("Bu Sentabr oyi")
+}else if(request == 10){
+    alert("Bu Oktabr oyi")
+}else if(request == 11){
+    alert("Bu Noyabr oyi")
+}else if(request == 12){
+    alert("Bu Dekabr oyi")
+}else{
+    alert("Noto'g'ri qiymat kiritildi")
+}
